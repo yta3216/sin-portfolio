56da117c028ad3f4dd98659fd91440815a3ac29c
yta3216 <yta3216@gmail.com>
2022-02-20 11:43:37 +0630

    finished v1

--- home.js
+++ home.js
@@ -7,17 +7,38 @@ const homemenu = document.querySelector(".home")
 const gallerybtn = document.querySelector(".galleryBtn")
 const gallerymenu = document.querySelector(".gallery")
 
-homebtn.onclick = function(){
+const card = document.querySelector(".card")
+const homecard = document.querySelector(".homecard")
+const sidecard = document.querySelector(".side-card")
+
+function homebtnactive(){
   menus.forEach((item)=>
   item.classList.remove('active-window'));
   homemenu.classList.add('active-window')
 }
-gallerybtn.onclick = function(){
+function gallerybtnactive(){
   menus.forEach((item)=>
   item.classList.remove('active-window'));
   gallerymenu.classList.add('active-window')
 }
 
+
 function linkToggle(){
   links.classList.toggle('activated')
 }
+function commInfoToggle(){
+  card.classList.toggle('card-toggle');
+  homecard.classList.toggle('homecard-toggle')
+  sidecard.classList.toggle('side-card-toggle')
+}
+
+function locationHashChanged() {
+  if (location.hash === '#home') {
+    homebtnactive()
+  } else if (location.hash === '#gallery'){
+    gallerybtnactive()
+  }
+}
+homebtn.onclick = homebtnactive
+gallerybtn.onclick = gallerybtnactive
+window.onhashchange = locationHashChanged;
